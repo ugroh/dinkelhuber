2c12f87d571e7f9589429ef30589fdfa73774e7b
yannikkellerde <yannik@kelnet.de>
2020-08-20 18:56:06 +0200

    test

--- test.py
+++ test.py
@@ -1,27 +1,24 @@
 from sqlitedict import SqliteDict
-from shove import Shove
+from go_game import Go_game,Rotater
 import time
 import random
 import numpy as np
 from tqdm import tqdm
 
-mydict = SqliteDict('./my_db.sqlite')
-shove_tb = Shove("lite://test.db")
-
-keys = np.array([],dtype=np.int64)
-while 1:
-    size = 1000
-    new_keys = np.random.randint(0,1<<63,size=size)
-    vals = np.random.randint(0,1<<63,size=size)
-    zippola = list(zip(new_keys,vals))
-    start = time.perf_counter()
-    for key,value in tqdm(zippola):
-        mydict[key]=value
-    mydict.commit()
-    print(f"sqlite insert: {time.perf_counter()-start}")
-    keys = np.concatenate((keys,new_keys))
-    np.random.shuffle(keys)
-    start = time.perf_counter()
-    for key in keys[:size]:
-        val = mydict[key]
-    print(f"sqlite lookup: {time.perf_counter()-start}")
+mydict = SqliteDict('./python_server/books/kyu_5.5_Japanese.sqlite')
+game = Go_game(Rotater(9),np.load("python_server/binfiles/zobrist.npy"))
+game.set_pos_from_str("""
+###########
+#         #
+#         #
+#      W  #
+#      WB #
+#  BBB  B #
+#   WB    #
+#   WWBB  #
+#     WW  #
+#         #
+###########""")
+print(mydict[118966001714952367])
+print(118966001714952367,game.do_hash())
+print(game)
